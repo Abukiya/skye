9681ee85880ddafbcfe5e2de71d6e4c34eda1086
Abukiya <yadaman36@gmail.com>
2026-03-17 20:59:50 +0300

    learining aobut ai in front-end

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,9 @@
+
 function Loader() {
   return (
-    <div className="text-center mt-6">
-      Loading...
+    <div className="flex items-center justify-center mt-4">
+      <div className="animate-spin  rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+      <span className="ml-2 text-gray-700">Loading...</span>
     </div>
   )
 }
